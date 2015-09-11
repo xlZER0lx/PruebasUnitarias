d4b9a55fde0235c3a471b8213e6911ef732b8f90
xlZER0lx <xl_z_lx@hotmail.com>
2015-09-11 11:17:01 -0500

    se agrego funcionalidad de sumar

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -2,13 +2,31 @@ import unittest
 
 class TestCalculadora(unittest.TestCase):
 
-    def test_sumar_de_2_mas_2(self):
-        calc = Calculadora()
+    def setUp(self):
+        self.calc = Calculadora()
 
-        resultado = calc.sumar(2, 2)
+    def test_sumar_de_2_mas_2(self):
+        resultado = self.calc.sumar(2, 2)
 
         self.assertEqual(4, resultado)
+        
+
+    def test_sumar_de_3_mas_3(self):
+        resultado = self.calc.sumar(3, 3)
+
+        self.assertEqual(6, resultado)
 
 
-if __main__ == ' __main__':
+    def test_sumar_de_5_mas_5(self):
+        resultado = self.calc.sumar(5, 5)
+
+        self.assertEqual(10, resultado)
+        
+
+class Calculadora():
+
+    def sumar(self, num1, num2):
+        return num1 + num2
+
+if __name__ == '__main__':
     unittest.main()
